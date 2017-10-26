8cb97d2369e454c9819158d61b86db108f21619c
Eduard Coll <ecollvila@gmail.com>
2017-10-26 21:19:49 +0200

    ADD: JGroupCluster with a demo of a simple cluster in Java.

---
+++ src/java/JGroupCluster.java
@@ -0,0 +1,59 @@
+import org.jgroups.Address;
+import org.jgroups.JChannel;
+import org.jgroups.View;
+
+/**
+ * Created by hocklo on 26/10/17.
+ */
+public class JGroupCluster {
+
+    private static final int MAX_ROUNDS = 1000;
+    private static final int SLEEP_TIME_IN_MILLIS = 1000;
+    private static final String IM_LEADER = "I'm (%s) the leader\n";
+    private static final String IM_NOT_LEADER = "I'm (%s) not the leader, the leader is %s\n";
+    private static final String CLUSTER_MEMBER = ">> %s";
+    private static final String CLUSTER_MEMBERS = "Cluster members";
+
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            JChannel channel = new JChannel();
+            channel.connect("The Test Cluster"); // Cluster name grouping the leaders with slaves.
+            for (int round = 0; round < MAX_ROUNDS; round++) {
+                checkLeaderStatus(channel);
+                sleep();
+            }
+            channel.close();
+        }
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(SLEEP_TIME_IN_MILLIS);
+        } catch (InterruptedException e) {
+            // Ignored
+        }
+    }
+
+    private static void checkLeaderStatus(JChannel channel) {
+        View view = channel.getView();
+        printMembers(view);
+        printWhoIam(channel, view);
+    }
+
+    private static void printWhoIam(JChannel channel, View view) {
+        Address address = view.getMembers()
+                .get(0);
+        if (address.equals(channel.getAddress())) {
+            System.out.println(String.format(IM_LEADER, channel.getAddress()));
+        } else {
+            System.out.println(String.format(IM_NOT_LEADER, channel.getAddress(), address));
+        }
+    }
+
+    private static void printMembers(View view) {
+        System.out.println(CLUSTER_MEMBERS);
+        for (Address address : view.getMembers()) {
+            System.out.println(String.format(CLUSTER_MEMBER, address));
+        }
+    }
+}
